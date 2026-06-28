8e6a0cd394ad8b81df2a976664fcf5bf42d0cdc9
getharshit <givenharshit@gmail.com>
2026-06-28 12:47:51 +0530

    try to resolve again

--- backend/server.js
+++ backend/server.js
@@ -9,13 +9,12 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use("/api/inngest", serve({ client: inngest, functions }));
 app.use(clerkMiddleware())
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/api/inngest', (req, res) => {
+  res.send('Inngest endpoint working')
 })
-
-app.use("/api/inngest", serve({ client: inngest, functions }));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, ()=>console.log(`Server is running on port ${PORT}`));
