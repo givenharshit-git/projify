2392bbe278c82801aa7fdcad4d9b2e306b2e414d
getharshit <givenharshit@gmail.com>
2026-06-29 19:50:17 +0530

    fixed fronted

--- backend/server.js
+++ backend/server.js
@@ -4,17 +4,19 @@ import cors from 'cors';
 import { clerkMiddleware } from '@clerk/express'
 import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js"
+import workspaceRouter from './routes/workspace.js';
+import { protect } from './middleware/auth.js';
 
 const app = express();
 
+// Middlewares
 app.use(express.json());
 app.use(cors());
 app.use(clerkMiddleware())
 app.use("/api/inngest", serve({ client: inngest, functions }));
 
-app.get('/', (req, res) => {
-  res.send('Inngest endpoint working')
-})
+// Routes
+app.use('/api/workspaces', protect, workspaceRouter);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, ()=>console.log(`Server is running on port ${PORT}`));
